3a271f3ed6065b2bad4db9b04788f736ff1c5948
Misha <73886737+misha244@users.noreply.github.com>
2021-08-18 00:29:33 +0100

    add context to server

--- server/server.js
+++ server/server.js
@@ -2,18 +2,18 @@ const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
 
-const db = require("./config/connection");
 const { typeDefs, resolvers } = require("./schemas");
-const { authMiddleware } = require("./utils/auth");
+const context = require("./context");
+
+const db = require("./config/connection");
 
 const app = express();
-
 const PORT = process.env.PORT || 3001;
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: authMiddleware,
+  context,
 });
 
 const startServer = async () => {
@@ -26,6 +26,7 @@ startServer();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
