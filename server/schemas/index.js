deab8375988f3887da36e3c10db54a47854b964c
Misha <73886737+misha244@users.noreply.github.com>
2021-08-19 21:53:51 +0100

    move typeDefs to index.js

--- server/schemas/index.js
+++ server/schemas/index.js
@@ -1,3 +1,51 @@
-const typeDefs = require("./typeDefs");
+const { gql } = require("apollo-server-express");
 
-module.exports = { typeDefs };
+const typeDefs = gql`
+  type Book {
+    bookId: ID!
+    authors: [String!]
+    description: String
+    title: String!
+    image: String
+    link: String
+  }
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int!
+    savedBooks: [Book]
+  }
+  type Auth {
+    token: ID!
+    user: User!
+  }
+  type Query {
+    me: User!
+  }
+  input LoginInput {
+    email: String!
+    password: String!
+  }
+  input AddUserInput {
+    username: String!
+    email: String!
+    password: String!
+  }
+  input SaveBookInput {
+    bookId: String!
+    authors: [String]
+    title: String!
+    description: String
+    image: String
+    link: String
+  }
+  type Mutation {
+    login(input: LoginInput!): Auth!
+    addUser(input: AddUserInput!): Auth!
+    saveBook(input: SaveBookInput!): User!
+    removeBook(bookId: ID!): User!
+  }
+`;
+
+module.exports = typeDefs;
