c205745b0d4a8b7e74613205ef8015abe9411d81
Misha <73886737+misha244@users.noreply.github.com>
2021-08-19 22:24:59 +0100

    format

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -5,7 +5,7 @@ const secret = process.env.JWT_SECRET || "my local secret";
 const expiration = process.env.JWT_EXPIRES_IN || "2h";
 
 const signToken = (payload) => {
-  return jwt.sign(payload, secret, { expiration });
+  return jwt.sign(payload, secret, { expiresIn: expiration });
 };
 
 const verifyToken = (token) => {
