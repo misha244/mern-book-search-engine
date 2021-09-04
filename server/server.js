0302f3a46a984dbbda47aa938a2045ccf61db2a4
Misha <73886737+misha244@users.noreply.github.com>
2021-09-04 15:54:02 +0100

    refactor and format

--- server/server.js
+++ server/server.js
@@ -2,15 +2,14 @@ const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
 
-const typeDefs = require("./schemas");
-const resolvers = require("./resolvers");
-
+const db = require("./config/connection");
+const typeDefs = require("./schemas/typeDefs");
+const resolvers = require("./resolvers/index");
+const { authMiddleware } = require("./utils/auth");
 const context = require("./context");
 
-const db = require("./config/connection");
-
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
 const server = new ApolloServer({
   typeDefs,
@@ -18,14 +17,17 @@ const server = new ApolloServer({
   context,
 });
 
+//start graphql server and implement express server as middleware
 const startServer = async () => {
   await server.start();
   server.applyMiddleware({ app });
+  return app;
 };
 
 startServer();
 
-app.use(express.urlencoded({ extended: true }));
+// express middleware
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
@@ -33,8 +35,17 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
+//connect to the database and run express server
 db.once("open", () => {
-  app.listen(PORT, () => {
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-  });
+  try {
+    app.listen(PORT, () => {
+      console.log(`🌍 Now listening on localhost:${PORT}`);
+      console.log(
+        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+      );
+    });
+  } catch (error) {
+    console.error(error.message);
+    console.error("Failed to run server");
+  }
 });
